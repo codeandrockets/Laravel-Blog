8e9baa17898abbe7401b3e586324876a6a9d4d1c
Steven Jay <stevenjasionowicz@gmail.com>
2016-05-01 17:01:56 +1200

    Add delete functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -123,6 +123,10 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        Session::flash('success', 'The Post was Deleted.');
+        return redirect()->route('post.index');
     }
 }
