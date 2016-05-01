2cee0ea39104083ba1b47e0121686dab27562062
Steven Jay <stevenjasionowicz@gmail.com>
2016-05-01 22:25:49 +1200

    Add blog post page

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,10 +9,19 @@ use App\Post;
 
 class BlogController extends Controller
 {
+
+	public function getIndex() {
+		$posts = Post::paginate(5);
+
+		return view('blog.index')->withPosts($posts);
+	}
+
     public function getSingle($slug) {
     	//get from db based on slug
     	$post = Post::where('slug', '=', $slug)->first();
     	// return a view and pass the object into it
     	return view('blog.single')->withPost($post);
     }
+
+
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,4 @@ Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getS
 
 //Blog Post Route
 Route::resource('post', 'PostController' );
+Route::get('blog', ['uses' =>'BlogController@getIndex', 'as' => 'blog.index']);
