392d3f966a7e202b8fba5f6e2f11697d98e83741
Steven Jay <stevenjasionowicz@gmail.com>
2016-05-01 14:55:50 +1200

    Add highlighted nav tabs

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -14,4 +14,29 @@
 			<hr>
 		</div>
 	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<table class="table">
+				<thead>
+					<th>#</th>
+					<th>Title</th>
+					<th>Body</th>
+					<th>Created At</th>
+					<th></th>
+				</thead>
+				<tbody>
+					@foreach($posts as $post)
+					<tr>
+						<th>{{ $post->id }}</th>
+						<td>{{ $post->title }}</td>
+						<td>{{ substr($post->body, 0, 120) }}{{ strlen($post->body) > 50 ? "..." : "" }}</td>
+						<td>{{ date('j M Y', strtotime($post->created_at)) }}</td>
+						<td><a href="{{ route('post.show', $post->id) }}" class="btn btn-default">View</a> <a href="{{ route('post.edit', $post->id) }}" class="btn btn-success">Edit</a></td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
 @endsection
